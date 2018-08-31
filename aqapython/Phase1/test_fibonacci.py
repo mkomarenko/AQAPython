4a6b34a8a7d014d62e50145e5646a111ebe0cdc7
maxim-komarenko <maxim.komarenko@microfocus.com>
2018-08-31 14:27:34 +0300

    fix text

--- aqapython/Phase1/test_fibonacci.py
+++ aqapython/Phase1/test_fibonacci.py
@@ -4,4 +4,4 @@ from .fibonacci import fib
 class TestFibonacci:
 
     def test_fib(self):
-        assert fib(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 346]
+        assert fib(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
