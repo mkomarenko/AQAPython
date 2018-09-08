2d7f3d20911caa7fd9be1ddbc3e673142d1f3fad
maxim-komarenko <maxim.komarenko@microfocus.com>
2018-09-08 15:21:06 +0300

    new create jira issue test

--- Phase2/tests/fixtures/json_fixtures.py
+++ Phase2/tests/fixtures/json_fixtures.py
@@ -5,3 +5,33 @@ class JsonFixtures:
             "username": "Maksym_Komarenko",
             "password": "Mk810426@odsu_"
             }
+
+    @staticmethod
+    def get_wrong_username():
+        return {
+            "username": "wrong",
+            "password": "Mk810426@odsu_"
+        }
+
+    @staticmethod
+    def get_wrong_password():
+        return {
+            "username": "Maksym_Komarenko",
+            "password": "wrong"
+        }
+
+    @staticmethod
+    def get_issue_json(summary):
+        return {
+            "fields": {
+                "project":
+                    {
+                        "key": "TEST"
+                    },
+                "summary": summary,
+                "description": "Creating of an issue using project keys and issue type names using the REST API",
+                "issuetype": {
+                    "name": "Bug"
+                }
+               }
+        }
--- Phase2/tests/test_login_to_jira.py
+++ Phase2/tests/test_login_to_jira.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from Phase2.tests.jira_globals import *
 from Phase2.tests.fixtures.json_fixtures import JsonFixtures
 
 
@@ -7,14 +8,15 @@ class TestJiraLogin:
 
     testdata = [
         (JsonFixtures.get_auth_data(), 200),
+        (JsonFixtures.get_wrong_username(), 401),
+        (JsonFixtures.get_wrong_password(), 401),
     ]
+
+    jira_url = base_url + '/rest/auth/1/session'
 
     @pytest.mark.parametrize("auth_data, expected", testdata)
     def test_login_to_jira(self, auth_data, expected):
-
-        jira_url='http://jira.hillel.it:8080/rest/auth/1/session'
-
         headers = {'Content-Type': 'application/json'}
-        r = requests.post(jira_url, json=auth_data, headers=headers)
+        r = requests.post(self.jira_url, json=auth_data, headers=headers)
         print("STATUS CODE: " + str(r.status_code))
-        assert r.status_code is expected
+        assert r.status_code == expected
