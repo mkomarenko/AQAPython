349876b63604d2661a271e896f4498e7a7caecd8
Maksym Komarenko <mkomarenko@lohika.com>
2018-10-01 11:55:48 +0300

    fix driver executable initialization

--- tests/base_test.py
+++ tests/base_test.py
@@ -4,8 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class BaseTest:
+    driver_executable_path = None
+
+    def init_diver_executable(self):
+        if BaseTest.driver_executable_path is None:
+            BaseTest.driver_executable_path = ChromeDriverManager().install()
+
     def setup_class(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.init_diver_executable(self)
+        self.driver = webdriver.Chrome(BaseTest.driver_executable_path)
         self.wait = WebDriverWait(self.driver, 30)
 
     def teardown_class(self):
